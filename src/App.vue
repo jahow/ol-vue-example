5a990d4315a0b0743bf8fb68567884afa9b5f081
Olivier Guyot <olivier.guyot@camptocamp.com>
2020-09-04 14:27:18 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <div class="cell-map">
+      <MapContainer/>
+    </div>
+    <div class="cell-edit">
+      hello world
+    </div>
+  </div>
+</template>
+
+<script>
+  import MapContainer from './components/MapContainer'
+
+  export default {
+    name: 'App',
+    components: {
+      MapContainer
+    }
+  }
+</script>
+
+<style>
+  html, body {
+    height: 100%;
+    margin: 0;
+  }
+
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    height: 100%;
+    display: grid;
+    grid-template-columns: 100vh;
+    grid-gap: 1rem;
+    padding: 1rem;
+    box-sizing: border-box;
+  }
+
+  .cell-map {
+    grid-column: 1;
+    grid-row-start: 1;
+    grid-row-end: 3;
+  }
+
+  .cell-edit {
+    grid-column: 2;
+    grid-row: 1;
+  }
+</style>
---
+++ src/components/MapContainer.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="ol-map"></div>
+</template>
+
+<style>
+  #ol-map {
+    width: 100%;
+    height: 100%;
+    border-radius: 4px;
+  }
+</style>
+
+<script>
+  import View from 'ol/View'
+  import Map from 'ol/Map'
+  import TileLayer from 'ol/layer/Tile'
+  import XYZ from 'ol/source/XYZ'
+
+  export default {
+    name: 'MapContainer',
+    components: {},
+    props: {},
+    mounted() {
+      new Map({
+        target: 'ol-map',
+        layers: [
+          new TileLayer({
+            source: new XYZ({
+              url: 'http://a.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png',
+            }),
+          }),
+        ],
+        view: new View({
+          zoom: 0,
+          center: [0, 0],
+          constrainResolution: true
+        }),
+      })
+    },
+  }
+</script>
